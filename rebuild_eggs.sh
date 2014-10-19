3634a5eaf28e67a68d76819d51838eb95dee365e
Stephen Hamer <stephen.hamer@gmail.com>
2014-10-19 16:45:18 -0400

    rebuilds_eggs.sh: use absolute path in eggs.pth file

--- rebuild_eggs.sh
+++ rebuild_eggs.sh
@@ -13,6 +13,6 @@ cp ver_a/dist/fooserv-ver_a-py*.egg builds/ver_a/
 cp ver_b/dist/fooserv-ver_b-py*.egg builds/ver_b/
 
 echo "updating eggs.pth files"
-ls builds/ver_a/fooserv-ver_a-py*.egg > builds/ver_a/eggs.pth
-ls builds/ver_b/fooserv-ver_b-py*.egg > builds/ver_b/eggs.pth
+find `pwd`/builds/ver_a/ -name "*.egg" > builds/ver_a/eggs.pth
+find `pwd`/builds/ver_b/ -name "*.egg" > builds/ver_b/eggs.pth
 
